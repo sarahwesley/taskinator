525565bafc17c4ce9be24e43ccda672e0274221a
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-13 13:46:48 -0500

    task form created. tasks can be added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
   event.preventDefault();
-   
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // create list item
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
+
+  // create div to hold task inof and add to list item
+  var taskInfoEl = document.createElement("div");
+  //give it a class name
+  taskInfoEl.className = "task-info";
+  // add html content to div
+  tastInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  tasksToDoEl.appendChild(listItemEl);
   }; 
 
 formEl.addEventListener("submit", createTaskHandler);

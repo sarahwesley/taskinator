c3ea159ee5c3594c10a0d5b7c9dcfa7507e0a884
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-13 14:21:40 -0500

    form validation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,15 @@ var taskFormHandler = function(event) {
       type: taskTypeInput
   };
 
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+  alert("You need to fill out the task form!");
+  return false;
+  }
+
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+  formEl.reset();
 };
 
 var createTaskEl = function (taskDataObj) {
